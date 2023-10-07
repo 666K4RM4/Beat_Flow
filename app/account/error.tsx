01c9d47caca4c93a255439f7516cd9e61b72e848
K4RM4 <animevostfr36@gmail.com>
2023-10-07 12:58:49 +0200

    final

---
+++ app/account/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Box from "@/components/Box";
+
+const Error = () =>{
+    <Box className="h-full flex items-center justify-center">
+        <div className="text-neutral-400">
+            something went wrong
+        </div>
+    </Box>
+}
+
+export default Error;
